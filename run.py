10a4791f2c52e51d0fe4527c2f0fb4f3105b8ed5
MalakarEresh <ereshman9890@gmail.com>
2025-08-25 18:32:46 +0545

    commit fina

--- run.py
+++ run.py
@@ -9,4 +9,6 @@ def make_shell_context():
     return {'db': db, 'User': User, 'Report': Report}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
+    app.run(host="0.0.0.0", port=port)
